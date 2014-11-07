86525dc7c62430754e5a8817270e4625a4f855e8
Chenjian0930 <chenjian@lianchuang.com>
2014-11-07 10:01:20 +0800

    。。。

--- WeiBo/WeiBo/General/GlobalNetWork.m
+++ WeiBo/WeiBo/General/GlobalNetWork.m
@@ -7,18 +7,8 @@
 //
 
 #import "GlobalNetWork.h"
-#import "AFNetworking.h"
 @implementation GlobalNetWork
 
-- (void)getPath:(NSString *)path
-     parameters:(NSDictionary *)parameters
-        success:(void (^)(AFHTTPRequestOperation *operation, id responseObject))success
-        failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
-{
-    NSURLRequest *request = [self requestWithMethod:@"GET" path:path parameters:parameters];
-    AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:success failure:failure];
-    [self enqueueHTTPRequestOperation:operation];
-}
 
 
 @end
--- WeiBo/WeiBo/ViewController.m
+++ WeiBo/WeiBo/ViewController.m
@@ -18,6 +18,40 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"%@",NSLocalizedString(@"loading",@""));
+    
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://httpbin.org/robots.txt"]];
+
+//    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+//    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+//        NSLog(@"Success: %@", operation.responseString);
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        NSLog(@"Failure: %@", error);
+//    }];
+//    [operation start];
+    
+    
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSDictionary *dic = @{@"source":@"iphone",@"sign":@"ios",@"password":@"111111",@"loginName":@"18625160299",@"versionNo":@"1"};
+    [manager POST:@"http://172.16.128.165:80/user/rest/login" parameters:dic success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        if ([responseObject isKindOfClass:[NSDictionary class]]) {
+            NSLog(@"NSDictionary") ;
+        }else if([responseObject isKindOfClass:[NSArray class]]){
+            NSLog(@"NSArray") ;
+        }
+        if ([NSJSONSerialization isValidJSONObject:responseObject]){
+            NSObject *obj  = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingAllowFragments error:nil];
+            NSLog(@"%@",obj);
+        }
+        NSLog(@"");
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"");
+    }];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
